da53dd6a116a0712ce91c7457b4216cd16d8ffca
Wii Yang <wii_yang@163.com>
2023-12-26 09:37:37 +0800

    init: 使用 vite 创建项目

---
+++ README.md
@@ -0,0 +1,7 @@
+##  项目创建流程
+
+### 一、使用 vite 创建项目
+
+```shell
+pnpm create vite my-vue-app --template vue-ts
+```
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import {ConfigEnv, defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig((config: ConfigEnv) => {
+  const { mode, command } = config
+  console.log(mode, command)
+  return {
+    plugins: [vue()],
+  }
+})
